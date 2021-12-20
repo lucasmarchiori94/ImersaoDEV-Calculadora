a532a29b35737305a14e60774484f529cca629e9
Lucas Marchiori <lucas.marchiori.santos@gmail.com>
2021-12-20 14:19:26 -0300

    modificaçao de teste

--- calc.js
+++ calc.js
@@ -5,7 +5,7 @@ var segundoValor = parseInt(prompt("Digite o Segundo valor:"))
 //Variavel de operação
 var operação = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma, 4 para subtração ")
 
-//condições
+//condiçõe
 if (operação == 1) {
   var resultado = primeiroValor / segundoValor
   document.write("<h2>" + primeiroValor + " / " + segundoValor + " = " + resultado + "</h2>")
